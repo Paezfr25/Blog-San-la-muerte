61aa60f0b9a0951836cede75f3b651cb09eff91d
francoPaez25 <soypaez17@hotmail.com>
2023-11-03 10:57:01 -0300

    cambio

--- script.js
+++ script.js
@@ -3,9 +3,20 @@ window.onscroll = function() {
   };
   
   function scrollFunction() {
+    var button = document.getElementById("btnVolverArriba");
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      document.querySelector('.back-to-top').classList.add('show');
+      button.style.display = "block";
     } else {
-      document.querySelector('.back-to-top').classList.remove('show');
+      button.style.display = "none";
     }
+  }
+  
+  // Hace scroll hacia arriba cuando se hace clic en el botón
+  document.getElementById("btnVolverArriba").onclick = function() {
+    scrollToTop();
+  };
+  
+  function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
   }
